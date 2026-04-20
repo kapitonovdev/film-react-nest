4c2aae4ac366a127eaf72661ad2a25e54cd97f98
Pavel Kapitonov <kapitonovdev@icloud.com>
2026-04-20 19:39:56 +0300

    Remove duplicated static content setup

--- backend/src/app.setup.ts
+++ backend/src/app.setup.ts
@@ -1,12 +1,6 @@
 import { INestApplication } from '@nestjs/common';
-import * as express from 'express';
-import * as path from 'node:path';
 
 export function configureApp(app: INestApplication): void {
-  app.use(
-    '/content',
-    express.static(path.join(__dirname, '..', 'public', 'content')),
-  );
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
 }
